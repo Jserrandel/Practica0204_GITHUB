5fadef8b188163c26f97f426f2c3bcf7168b3686
Jserrandel <jserrandel@educacion.navarra.es>
2024-10-28 11:22:24 +0100

    Ejercicio 1

---
+++ Ejercicio1.py
@@ -0,0 +1,5 @@
+#Escribir un programa que almacene las asignaturas de un curso
+#(por ejemplo Matemáticas, Física, Química, Historia y Lengua)
+#en una lista y la muestre por pantalla.
+Asignaturas = ["Quimica", "Matematicas", "Euskera", "Historia", "Fisica", "Lengua castellana"]
+print(Asignaturas)

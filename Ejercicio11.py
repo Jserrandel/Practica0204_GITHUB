64abc6e5de37e2a0e6e2dd921c706006046b2f9d
Jserrandel <jserrandel@educacion.navarra.es>
2024-11-04 11:30:11 +0100

    Ejercicio 11

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -3,14 +3,14 @@ a = ((1, 2, 3),
 b = ((-1, 0),
      (0, 1),
      (1,1))
-result = [[0,0],
+resultado = [[0,0],
           [0,0]]
 for i in range(len(a)):
     for j in range(len(b[0])):
         for k in range(len(b)):
-            result[i][j] += a[i][k] * b[k][j]
+            resultado[i][j] += a[i][k] * b[k][j]
 for i in range(len(result)):
-    result[i] = tuple(result[i])
-result = tuple(result)
-for i in range(len(result)):
+    resultado[i] = tuple(result[i])
+result = tuple(resultado)
+for i in range(len(resultado)):
     print(result[i])
